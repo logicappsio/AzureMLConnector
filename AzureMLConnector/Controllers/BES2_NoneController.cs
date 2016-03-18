4881c520524318b847f1b946e79a39531dd61c67
Jeff Hollan <jeffhollan@outlook.com>
2016-03-18 11:18:51 -0700

    modified to api apps v2

--- AzureMLConnector/Controllers/BES2_NoneController.cs
+++ AzureMLConnector/Controllers/BES2_NoneController.cs
@@ -16,24 +16,24 @@ namespace AzureMLConnector.Controllers
         [Swashbuckle.Swagger.Annotations.SwaggerResponseRemoveDefaults]
         [HttpPost, Route("api/None")]
         public async Task<HttpResponseMessage> Post(
-             [Metadata("API POST URL", "Web Service Request URI")] string _API_URL,
-             [Metadata("API Key", "Web Service API Key")] string _API_Key,             
-             [Metadata(FriendlyName = "Global Parameters Keys", Description = "Comma separated list of parameters", Visibility = VisibilityType.Advanced)] string _GlobalKeys = "",
-             [Metadata(FriendlyName = "Global Parameters Values", Description = "Comma separated list of values", Visibility = VisibilityType.Advanced)] string _GlobalValues = ""
+             [Metadata("API POST URL", "Web Service Request URI")] string API_URL,
+             [Metadata("API Key", "Web Service API Key")] string API_Key,             
+             [Metadata(FriendlyName = "Global Parameters Keys", Description = "Comma separated list of parameters", Visibility = VisibilityType.Advanced)] string GlobalKeys = "",
+             [Metadata(FriendlyName = "Global Parameters Values", Description = "Comma separated list of values", Visibility = VisibilityType.Advanced)] string GlobalValues = ""
 
             )
         {
             BES_None Obj = new BES_None
             {
-                API_URL = _API_URL,
-                API_Key = _API_Key,                
-                GlobalKeys = _GlobalKeys,
-                GlobalValues = _GlobalValues
+                API_URL = API_URL,
+                API_Key = API_Key,                
+                GlobalKeys = GlobalKeys,
+                GlobalValues = GlobalValues
             };
 
             BatchScoreStatus result = await BatchExecutionService.InvokeBatchExecutionService(Obj);
             HttpResponseMessage response = Request.CreateResponse<BatchScoreStatus>(HttpStatusCode.Accepted, result);
-            response.Headers.Location = new Uri(string.Format("https://" + Request.RequestUri.Authority + "/api/CheckStatus?url={0}&api={1}", WebUtility.UrlEncode(result.JobLocation), WebUtility.UrlEncode(_API_Key)));
+            response.Headers.Location = new Uri(string.Format("https://" + Request.RequestUri.Authority + "/api/CheckStatus?url={0}&api={1}", WebUtility.UrlEncode(result.JobLocation), WebUtility.UrlEncode(API_Key)));
             response.Headers.Add("Retry-after", "30");
             return response;
         }
--- AzureMLConnector/Controllers/BES4_OutputController.cs
+++ AzureMLConnector/Controllers/BES4_OutputController.cs
@@ -18,33 +18,33 @@ namespace AzureMLConnector.Controllers
         [Swashbuckle.Swagger.Annotations.SwaggerResponseRemoveDefaults]
         [HttpPost, Route("api/OnlyOutput")]
         public async Task<HttpResponseMessage> Post(
-              [Metadata("API POST URL", "Web Service Request URI")] string _API_URL,
-              [Metadata("API Key", "Web Service API Key")] string _API_Key,
+              [Metadata("API POST URL", "Web Service Request URI")] string API_URL,
+              [Metadata("API Key", "Web Service API Key")] string API_Key,
               
-              [Metadata("Storage Account Name (Output)", "Azure Storage Account Name. Leave blank if same with Input")] string _Output_AccountName,
-              [Metadata("Storage Account Key (Output)", "Azure Storage Account Key. Leave blank if same with Input")] string _Output_AccountKey,
-              [Metadata("Storage Container Name (Output)", "Azure Storage Container Name. Leave blank if same with Input")] string _Output_Container,
-              [Metadata("Blob Name (Output)", "Azure Storage Blob Name. Include file extention. Leaving blank will set it default name")] string _Output_Blob,
-              [Metadata(FriendlyName = "Global Parameters Keys", Description = "Comma separated list of parameters", Visibility = VisibilityType.Advanced)] string _GlobalKeys = "",
-              [Metadata(FriendlyName = "Global Parameters Values", Description = "Comma separated list of values", Visibility = VisibilityType.Advanced)] string _GlobalValues = ""
+              [Metadata("Storage Account Name (Output)", "Azure Storage Account Name. Leave blank if same with Input")] string Output_AccountName,
+              [Metadata("Storage Account Key (Output)", "Azure Storage Account Key. Leave blank if same with Input")] string Output_AccountKey,
+              [Metadata("Storage Container Name (Output)", "Azure Storage Container Name. Leave blank if same with Input")] string Output_Container,
+              [Metadata("Blob Name (Output)", "Azure Storage Blob Name. Include file extention. Leaving blank will set it default name")] string Output_Blob,
+              [Metadata(FriendlyName = "Global Parameters Keys", Description = "Comma separated list of parameters", Visibility = VisibilityType.Advanced)] string GlobalKeys = "",
+              [Metadata(FriendlyName = "Global Parameters Values", Description = "Comma separated list of values", Visibility = VisibilityType.Advanced)] string GlobalValues = ""
 
              )
         {
             BES_Output Obj = new BES_Output
             {
-                API_URL = _API_URL,
-                API_Key = _API_Key,
+                API_URL = API_URL,
+                API_Key = API_Key,
                 
-                Output_AccountName = _Output_AccountName,
-                Output_AccountKey = _Output_AccountKey,
-                Output_Container = _Output_Container,
-                Output_Blob = _Output_Blob,
-                GlobalKeys = _GlobalKeys,
-                GlobalValues = _GlobalValues
+                Output_AccountName = Output_AccountName,
+                Output_AccountKey = Output_AccountKey,
+                Output_Container = Output_Container,
+                Output_Blob = Output_Blob,
+                GlobalKeys = GlobalKeys,
+                GlobalValues = GlobalValues
             };
             BatchScoreStatus result = await BatchExecutionService.InvokeBatchExecutionService(Obj);
             HttpResponseMessage response = Request.CreateResponse<BatchScoreStatus>(HttpStatusCode.Accepted, result);
-            response.Headers.Location = new Uri(string.Format("https://" + Request.RequestUri.Authority + "/api/CheckStatus?url={0}&api={1}", WebUtility.UrlEncode(result.JobLocation), WebUtility.UrlEncode(_API_Key)));
+            response.Headers.Location = new Uri(string.Format("https://" + Request.RequestUri.Authority + "/api/CheckStatus?url={0}&api={1}", WebUtility.UrlEncode(result.JobLocation), WebUtility.UrlEncode(API_Key)));
             response.Headers.Add("Retry-after", "30");
             return response;
         }
